42ce63cdd71db2846cbde9ab6f96df1711af09d1
cleriphil <cleri.phil@gmail.com>
2015-08-15 17:16:34 -0700

    begin styling of forms; add btn styles to action buttons

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,6 @@
 class Artist < ActiveRecord::Base
   validates :name, :presence => true
+  validates :name, :uniqueness => true
   validates :nationality, :presence => true
   validates :year_of_birth, :presence => true
   has_many :lots
